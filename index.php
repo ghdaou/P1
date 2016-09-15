e1a1c92bee33aa419d3218341cc9ce9a27c0512c
George Daou <georgehdaou@yahoo.com>
2016-09-15 07:53:17 -0400

    Adding author'r photo

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
 <head>
 <title>Project 1</title>
 
-<link rel="stylesheet" type="text/css" href="mystyle.css">
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
 
    <h1>GEORGE DAOU</h1>
 
-   <p><img src="myphoto.jpg" alt="George's Photo" style="width:304px;height:228px;"></p>
+     <p><img src="/images/GeorgeDaou.jpg" alt="George's Photo" style="width:304px;height:228px;"></p>
 
    <h2>P2</h2>
-   <p>Live: [ Link ]</p>
-   <p>Github: [ Link ]</p>
+     <p>Live: [ Link ]</p>
+     <p>Github: [ Link ]</p>
 
    <h2>P3</h2>
-   <p>Live: [ Link ]</p>
-   <p>Github: [ Link ]</p>
+     <p>Live: [ Link ]</p>
+     <p>Github: [ Link ]</p>
 
    <h2>P4</h2>
-   <p>Live: [ Link ]</p>
-   <p>Github: [ Link ]</p>
+     <p>Live: [ Link ]</p>
+     <p>Github: [ Link ]</p>
 
 </body>
 </html>
